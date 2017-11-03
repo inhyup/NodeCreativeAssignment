a3691f08eae98985b3f71128924d2140ce04c513
inhyup <hyup3569@gmail.com>
2017-11-02 19:51:30 -0600

    change error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('weather.html', { title: 'Express' });
+  res.sendFile('weather.html', { root:  'public' });
 });
 
 module.exports = router;
