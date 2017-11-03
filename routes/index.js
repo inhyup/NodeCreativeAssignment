dbf641ae566e33e9755f2944bcfba1fba0003b83
inhyup <hyup3569@gmail.com>
2017-11-02 19:53:46 -0600

    add index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('weather.html', { root:  'public' });
+});
+
+var count=1;
+io.on('connection', function(socket){
+  console.log('user connected: ', socket.id);
+  var name = "user" + count++;
+  io.to(socket.id).emit('change name',name);
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected: ', socket.id);
+  });
+  
+  socket.on('send message', function(name,text){
+    var msg = name + ' : ' + text;
+    console.log(msg);
+    io.emit('receive message', msg);
+  });
+});
+
+http.listen('3000', function(){
+  console.log("server on!");
 });
 
 module.exports = router;
